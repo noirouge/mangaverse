96693231f25ba642e82c98a1b300d6d7690d5342
darlinsantana <116294763+darlinsantana@users.noreply.github.com>
2024-07-01 14:18:42 -0400

    Working list carrousel trending

---
+++ src/components/TrendingCarrousel.vue
@@ -0,0 +1,219 @@
+<template>
+<div class="trending">
+            <div class="trending-carrousel">
+              <img src="../assets/imgs/trending-carrousel-photo.jpg" />
+              <div class="carrousel-overlay">
+                <div class="carrousel-content">
+                  <div class="carrousel-content-tags">
+                    <div class="tag" style="background: #1464FF; margin-left: 0;">Manga</div>
+                    <div class="tag" style="background: red;">Seinen</div>
+                    <div class="tag" style="background: #00A707;">On Going</div>
+                  </div>
+                  <div class="carrousel-content-title">
+                    Kimetsu No Yaiba
+                  </div>
+                  <div class="carrousel-content-chapters">
+                    Chapters: 130
+                  </div>
+                  <div class="carrousel-content-info">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia beatae molestiae eius ea dolore ducimus,
+                    nisi ipsa, dignissimos itaque fugit debitis, voluptatum ipsum repudiandae voluptatem culpa distinctio
+                    quas nobis a explicabo nam! Officia quisquam fugit deleniti? Voluptatibus quisquam quod natus illum
+                    corporis consequuntur reiciendis, quae saepe labore. Molestias, explicabo ab!
+                  </div>
+                  <div class="carrousel-content-points" style="padding-left: 0">
+                    <div class="carrousel-content-point selected"></div>
+                    <div class="carrousel-content-point"></div>
+                    <div class="carrousel-content-point"></div>
+                    <div class="carrousel-content-point"></div>
+                    <div class="carrousel-content-point"></div>
+  
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="trending-list">
+              <div class="trending-list-item selected">
+                <div class="list-item-number">
+                    1
+                </div>
+                <div class="list-item-photo">
+                    <img src="../assets/imgs/portadas/demon-slayer.jpg" />
+                </div>
+                <div class="list-item-content">
+                    <div class="list-item-content-title">
+                        Kimetsu No Yaiba
+                    </div>
+                    <div class="list-item-content-rate">
+                        <div class="list-item-content-rate-views">
+                            100,000
+                        </div>
+                        <div class="list-item-content-rate-stars">
+                            XXXXX
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+/* CARROUSEL:BEGIN */
+.trending {
+  display: flex;
+}
+
+.trending-carrousel {
+  display: flex;
+  width: 800px;
+  height: 450px;
+}
+
+.carrousel-overlay{
+  position: absolute;
+    height: 450px;
+    align-content: flex-end;
+}
+
+.carrousel-content {
+  padding-bottom: 10px;
+    border-radius: 0 0 25px 25px;
+    height: 235px;
+    width: 795px;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.662), rgba(0, 0, 0, 0.849), black);
+    align-content: flex-end;
+  }
+
+/* .carrousel-content *{
+  margin-left: 10px;
+} */
+
+.carrousel-content-title{
+  margin-left: 10px;
+  font-size: 28px;
+  /* font-weight: bold; */
+  font-family: 'Trade Winds';
+}
+
+.carrousel-content-chapters{
+  margin-left: 10px;
+  font-size: 15px;
+  font-family: 'Times New Roman', Times, serif;
+  font-weight: bold;
+    color: #ffffffc7;
+    width: fit-content;
+    padding: 1px 5px;
+    border-radius: 25px;
+    background: rgb(255 255 255 / 14%);
+}
+
+.carrousel-content-info{
+  margin-left: 10px;
+  color: rgb(211, 211, 211);
+}
+
+.carrousel-content-points{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carrousel-content-point{
+  background: #6d6d6d80;
+    margin-left: 5px;
+    height: 10px;
+    width: 10px;
+    border-radius: 50px;
+}
+.carrousel-content-point.selected{
+  background: rgba(255, 255, 0, 0.516);
+}
+.carrousel-content-tags {
+  margin-left: 10px;
+  display: flex;
+}
+
+.trending-carrousel img {
+  width: max-content;
+    border-radius: 25px;
+    border-left: solid 3px rgba(0, 0, 0, 0.495);
+    border-bottom: solid 5px rgba(0, 0, 0, 0.705);
+}
+
+/* CARROUSEL:END */
+/* LIST:BEGIN */
+.trending-list {
+  border-left: solid 1px #00000063;
+    border-bottom: solid 2px #00000047;
+  border-radius: 25px;
+    background: #00000070;
+    display: flex;
+    width: 25%;
+    justify-content: center;
+}
+.trending-list-item{
+    padding: 3px;
+    margin-top: 11px;
+    border-radius: 15px;
+    width: 95%;
+    height: 80px;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.trending-list-item.selected{
+    background: #ffffff0d;
+}
+
+.list-item-number{
+    display: flex;
+    width: 50px;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    font-size: 80px;
+}
+
+.list-item-photo{
+    height: 75px;
+    width: 50px;
+}
+
+.list-item-photo img{
+    border-radius: 5px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+}
+
+.list-item-content{
+    display: flex;
+    flex-direction: column;
+}
+
+.list-item-content-title{
+    display: flex;
+}
+
+.list-item-content-rate{
+    display: flex;
+}
+
+.list-item-content-rate-views{
+    display: flex;
+}
+
+.list-item-content-rate-stars{
+    display: flex;
+}
+
+/* LIST:BEGIN */
+</style>
--- src/core/layouts/NavBar.vue
+++ src/core/layouts/NavBar.vue
@@ -1,15 +1,17 @@
 <template >
     <div class="navbar">
         <div class="brand-logo">MANGAVERSE</div>
-        <div class="nav-items">
+        <div class="nav">
             <div class="nav-search">
                 <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
                 <input class="search-input"  placeholder="Search Manga..."/>
             </div>
-            <div class="nav-item selected">HOME</div>
-            <div class="nav-item">LIBRARY</div>
-            <div class="nav-item">READING</div>
-            <div class="nav-item">MY LIST</div>
+            <div class="nav-items">
+                <div class="nav-item selected">HOME</div>
+                <div class="nav-item">LIBRARY</div>
+                <div class="nav-item">READING</div>
+                <div class="nav-item">MY LIST</div>
+            </div>
             <div class="nav-profile-section">
                 <div class="nav-notification">
 
@@ -38,7 +40,7 @@ console.log("HELLO NAVBAR");
     background: -webkit-linear-gradient(#ffffff, #dddddd, #0D0D0D);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
-    font-size: 40px;
+    font-size: 28px;
 }
 
 .navbar{
@@ -48,8 +50,15 @@ console.log("HELLO NAVBAR");
 
 .nav-items{
     display: flex;
+    width: 100%;
+}
+
+.nav{
+    width: 100%;
+    display: flex;
     margin-left: 25px;
     align-items: center;
+    justify-content: space-between;
 }
 
 .nav-search{
@@ -58,21 +67,18 @@ console.log("HELLO NAVBAR");
 }
 
 .search-icon{
-    
     color: #8B8B8B;
-    margin-left: 4px;
-    margin-top: 9px;
+    margin-left: 1px;
+    margin-top: 5px;
     position: absolute;
-    height: 26px;
+    height: 20px;
     width: 32px;
-
 }
 
 .search-input{
-    
-    padding-left: 38px;
-    font-size: 24px;
-    height: 40px;
+    padding-left: 31px;
+    font-size: 20px;
+    height: 28px;
     border-radius: 12px;
     width: 400px;
     border: 0;
@@ -83,7 +89,7 @@ console.log("HELLO NAVBAR");
 
 .nav-item{
     font-family: 'Times New Roman', Times, serif;
-    font-size: 24px;
+    font-size: 18px;
     font-weight: bold;
     margin-left: 40px;
 }
