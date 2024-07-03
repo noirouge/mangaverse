5b11a31d717bac031756995e4a5756393dfb7246
darlinsantana <116294763+darlinsantana@users.noreply.github.com>
2024-07-03 14:36:23 -0400

    content-carrousel

--- src/components/TrendingCarrousel.vue
+++ src/components/TrendingCarrousel.vue
@@ -1,45 +1,71 @@
 <template>
-<div class="trending">
-            <div class="trending-carrousel">
-              <img src="../assets/imgs/trending-carrousel-photo.jpg" />
-              <div class="carrousel-overlay">
-                <div class="carrousel-content">
-                  <div class="carrousel-content-tags">
-                    <div class="tag" style="background: #1464FF; margin-left: 0;">Manga</div>
-                    <div class="tag" style="background: red;">Seinen</div>
-                    <div class="tag" style="background: #00A707;">On Going</div>
-                  </div>
-                  <div class="carrousel-content-title">
-                    Kimetsu No Yaiba
-                  </div>
-                  <div class="carrousel-content-chapters">
-                    Chapters: 130
-                  </div>
-                  <div class="carrousel-content-info">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia beatae molestiae eius ea dolore ducimus,
-                    nisi ipsa, dignissimos itaque fugit debitis, voluptatum ipsum repudiandae voluptatem culpa distinctio
-                    quas nobis a explicabo nam! Officia quisquam fugit deleniti? Voluptatibus quisquam quod natus illum
-                    corporis consequuntur reiciendis, quae saepe labore. Molestias, explicabo ab!
-                  </div>
-                  <div class="carrousel-content-points" style="padding-left: 0">
-                    <div class="carrousel-content-point selected"></div>
-                    <div class="carrousel-content-point"></div>
-                    <div class="carrousel-content-point"></div>
-                    <div class="carrousel-content-point"></div>
-                    <div class="carrousel-content-point"></div>
-  
-                  </div>
-                </div>
-              </div>
-            </div>
-           <TrendingCarrouselList />
+  <div class="trending">
+    <div class="trending-carrousel">
+      <div class="carrousel-img" id="carrousel-img">
+        <div class="img" v-for="item in trendingCarrouselData" :key="item.id">
+          <img :alt="item.title" :src="item.cover" />
+        </div>
+      </div>
+      <div class="carrousel-overlay">
+        <div class="carrousel-content">
+          <div class="carrousel-content-tags">
+            <div v-for="tag,index of trendingSelected.tags" :key="tag.name" class="tag" style="background: #1464FF; margin-left: 0;" :style="{background: tag.color, 'margin-left': (!index?0:'10px')}">{{ tag.name }}</div>
           </div>
+          <div class="carrousel-content-title">
+           {{ trendingSelected.title }}
+          </div>
+          <div class="carrousel-content-chapters">
+            Chapters: {{ trendingSelected.chapters }}
+          </div>
+          <div class="carrousel-content-info">
+            {{ trendingSelected.description }}
+          </div>
+          <div class="carrousel-content-points" style="padding-left: 0">
+            <div class="carrousel-content-point" :class="{selected: trendingSelected.id === 1}"></div>
+            <div class="carrousel-content-point" :class="{selected: trendingSelected.id === 2}"></div>
+            <div class="carrousel-content-point" :class="{selected: trendingSelected.id === 3}"></div>
+            <div class="carrousel-content-point" :class="{selected: trendingSelected.id === 4}"></div>
+            <div class="carrousel-content-point" :class="{selected: trendingSelected.id === 5}"></div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <TrendingCarrouselList />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue';
 import TrendingCarrouselList from '../components/TrendingCarrouselList.vue'
+import useExampleData from '../core/example/useExampleData.ts'
+const { trendingCarrouselData } = useExampleData();
+const carrouselImgContainer = ref<any>(null);
+const trendingSelected = reactive({
+id: trendingCarrouselData[0].id,
+chapters: trendingCarrouselData[0].chapters,
+description: trendingCarrouselData[0].description,
+portrait: trendingCarrouselData[0].portrait,
+stars: trendingCarrouselData[0].stars,
+tags: trendingCarrouselData[0].tags,
+title: trendingCarrouselData[0].title,
+views: trendingCarrouselData[0].views,
+cover: trendingCarrouselData[0].cover,
+});
+
+function carrouselScroll(args:any){
+console.log("carrouselScroll", args);
+};
 
 
+onMounted(() => {
+  carrouselImgContainer.value = document.getElementById('carrousel-img');
+  // carrouselImgContainer.value.scrollLeft = 10;
+  carrouselImgContainer.value.onscroll = carrouselScroll;
+  // carrouselImgContainer.value.ondrag = (e: any) => onCarrouselMouseEvent("ondrag", e);
+  // carrouselImgContainer.value
+  console.log("MONTADO", carrouselImgContainer.value);
+});
 
 </script>
 
@@ -50,82 +76,122 @@ import TrendingCarrouselList from '../components/TrendingCarrouselList.vue'
 }
 
 .trending-carrousel {
+
   display: flex;
   width: 800px;
   height: 450px;
 }
 
-.carrousel-overlay{
+.carrousel-img {
+  overflow: hidden;
+  display: flex;
+  width: 790px;
+  height: 449px;
+  border-radius: 25px;
+  border-left: solid 3px rgba(0, 0, 0, 0.495);
+  border-bottom: solid 5px rgba(0, 0, 0, 0.705);
+}
+
+.carrousel-img .img {
+  position: relative;
+  left: 0px;
+  width: 796px;
+  height: 450px;
+}
+
+.carrousel-img img {
+  height: 450px;
+  width: max-content;
+  border-radius: 25px;
+  border-left: solid 3px rgba(0, 0, 0, 0.495);
+  border-bottom: solid 5px rgba(0, 0, 0, 0.705);
+}
+
+.carrousel-overlay {
+  pointer-events: none;
   position: absolute;
-    height: 450px;
-    align-content: flex-end;
+  height: 450px;
+  align-content: flex-end;
 }
 
 .carrousel-content {
   padding-bottom: 10px;
-    border-radius: 0 0 25px 25px;
-    height: 235px;
-    width: 795px;
-    background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.662), rgba(0, 0, 0, 0.849), black);
-    align-content: flex-end;
-  }
+  border-radius: 0 0 25px 25px;
+  height: 235px;
+  width: 795px;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.662), rgba(0, 0, 0, 0.849), black);
+  align-content: flex-end;
+}
 
 /* .carrousel-content *{
   margin-left: 10px;
 } */
 
-.carrousel-content-title{
-  margin-left: 10px;
-  font-size: 28px;
-  /* font-weight: bold; */
-  font-family: 'Trade Winds';
+.carrousel-content-title {
+  white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: 58%;
+    margin-left: 10px;
+    font-size: 28px;
+    /* font-weight: bold; */
+    font-family: 'Trade Winds';
 }
 
-.carrousel-content-chapters{
+.carrousel-content-chapters {
   margin-left: 10px;
   font-size: 15px;
   font-family: 'Times New Roman', Times, serif;
   font-weight: bold;
-    color: #ffffffc7;
-    width: fit-content;
-    padding: 1px 5px;
-    border-radius: 25px;
-    background: rgb(255 255 255 / 14%);
+  color: #ffffffc7;
+  width: fit-content;
+  padding: 1px 5px;
+  border-radius: 25px;
+  background: rgb(255 255 255 / 14%);
 }
 
-.carrousel-content-info{
+.carrousel-content-info {
+  height: 80px;
   margin-left: 10px;
   color: rgb(211, 211, 211);
 }
 
-.carrousel-content-points{
+.carrousel-content-points {
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.carrousel-content-point{
+.carrousel-content-point {
   background: #6d6d6d80;
-    margin-left: 5px;
-    height: 10px;
-    width: 10px;
-    border-radius: 50px;
+  margin-left: 5px;
+  height: 10px;
+  width: 10px;
+  border-radius: 50px;
 }
-.carrousel-content-point.selected{
+
+.carrousel-content-point.selected {
   background: rgba(255, 255, 0, 0.516);
 }
+
 .carrousel-content-tags {
   margin-left: 10px;
   display: flex;
 }
 
-.trending-carrousel img {
+/* .trending-carrousel img {
   width: max-content;
     border-radius: 25px;
     border-left: solid 3px rgba(0, 0, 0, 0.495);
     border-bottom: solid 5px rgba(0, 0, 0, 0.705);
-}
+} */
 
 /* CARROUSEL:END */
-
+/* CARROUSEL-MOBILE-VERSION:BEGIN */
+@media only screen and (max-width: 600px){
+  .carrousel-img{
+    overflow: auto;
+  }
+}
+/* CARROUSEL-MOBILE-VERSION:END */
 </style>
