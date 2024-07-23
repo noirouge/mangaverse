b2c10f2f71d35bc9335b4b1c88f0bf8e7b2f6332
darlinsantana <116294763+darlinsantana@users.noreply.github.com>
2024-07-23 12:05:02 -0400

    Photos in public for temporal build

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './core/layouts/NavBar.vue';
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <header>
  <NavBar />
   </header>
-
-  <RouterView />
+  <HomeView />
+  <!-- <RouterView /> -->
 </template>
